043b3a73373bd9cffdce05a5e027182f58395c5c
eraguzy <chekhab.elias@gmail.com>
2024-04-22 14:28:08 +0200

    update index php

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
-
 <html>
-    <head>
-        <title>Blob</title>
-    </head>
-    <body>
-        <div>hello world</div>
-    </body>
-    <footer></footer>
+<head>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <!-- Bandeau de navigation -->
+    <nav class="bandeau">
+        <input class="bouton" value="Connexion"/>
+    </nav>
+    
+    <!-- Contenu de la page -->
+    <div class="contenu">
+        <h1>Blob : Le premier site de rencontre pour les propriétaires de poissons rouges</h1>
+    </div>
+</body>
 </html>
