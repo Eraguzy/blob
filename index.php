a3b50a2dd3c2485e210bb36645767e7aadb62c29
louevaberanger <133343765+louevaberanger@users.noreply.github.com>
2024-04-28 22:48:44 +0200

    ajout favicon, titre, photo, description, centrage du texte

--- index.php
+++ index.php
@@ -2,19 +2,32 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="icon" type="image/png" href="logo.png">
+    <title>Blob</title>
     </head>
     <body>
         <nav class="bandeau">
             <img src="logo.png" class="img">
             <div class="bandeautitle">BLOB</div>
-            <input id="deuxboutons" type="button" class="bouton" value="Inscription"/>
+            <input id="deuxboutons" type="button" class="bouton" value="Inscription" onclick="linkopener('page_inscription.php')"/>
             <input type="button" class="bouton" value="Connexion" onclick="linkopener('page_connexion.php')"/>
         </nav>
             
         <div class="contenu">
             <h1>Blob : Le premier site de rencontre pour les propriétaires de poissons rouges</h1>
+        <div class="imagelogo">
+            <img src="logo.png">
+        </div>
+        <input type="button" class="bouton" value="Rejoignez-nous" id="boutongauche" onclick="linkopener('page_inscription.php')"/>
+        <input type="button" class="bouton" value="Rejoignez-nous" id="boutondroit" onclick="linkopener('page_inscription.php')"/>
+        <h2>Trouvez l'amour avec un compagnon à nageoires ! Rejoignez Blob, le site de rencontre pour les propriétaires de poissons rouges. Plongez dans une expérience unique où l'amour et les écailles se rencontrent.</h2>
         </div>
         <script src="script.js" type="text/javascript"></script>
     </body>
+    <footer>
+    <p>©2024 Tous droits réservés. Blob Corporate. Site réalisé par Louèva BERANGER, Lucas GOURNAY, Elias CHEKHAB.</p>
+    </footer>
+
 </html>
+
