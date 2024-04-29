4bad198e41db6a6285f9b639b97e51af93d4b28e
louevaberanger <133343765+louevaberanger@users.noreply.github.com>
2024-04-29 22:32:59 +0200

    Update page_connexion.php

--- page_connexion.php
+++ page_connexion.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="styles.css">
     <link rel="icon" href="logo.png">
     <title>Blob</title>
 </head>
