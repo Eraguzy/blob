9fbc6229233dac05405f9069ba396664a14b5c91
eraguzy <chekhab.elias@gmail.com>
2024-04-30 10:00:41 +0200

    modulation code css
    - css globaux à inclure
    - un fichier css correspond à chaque fichier php + chacun de ces fichiers css incluera les choses globales

--- accueil.php
+++ accueil.php
@@ -2,7 +2,7 @@
 <html lang="fr">
 <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="stylesheet" type="text/css" href="styles/accueil.css">
     <title>Blob</title>
     <link rel="icon" href="logo.png">
 </head>
