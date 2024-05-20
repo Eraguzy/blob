e262eef823b2491e9900799ad47cd4190efb4585
Loueva <133343765+louevaberanger@users.noreply.github.com>
2024-05-20 15:07:24 +0200

    problème de script réglé

--- souscription.php
+++ souscription.php
@@ -79,7 +79,48 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </div>
     </div>
 
-    <script src="script.js" type="text/javascript"></script>
+    <script>
+          
+    function linkopener(a){
+    window.open(a,'_self');
+}
+const element1 = document.querySelector('.discover');
+const encart1 = document.querySelector('.encart1');
+
+const element2 = document.querySelector('.classic');
+const encart2 = document.querySelector('.encart2');
+
+const element3 = document.querySelector('.privilege');
+const encart3 = document.querySelector('.encart3');
+
+element1.addEventListener('mouseover', () => {
+  encart1.style.display = 'block';
+});
+
+element1.addEventListener('mouseout', () => {
+  encart1.style.display = 'none';
+});
+
+element2.addEventListener('mouseover', () => {
+    encart2.style.display = 'block';
+  });
+  
+element2.addEventListener('mouseout', () => {
+    encart2.style.display = 'none';
+  });
+
+
+element3.addEventListener('mouseover', () => {
+    encart3.style.display = 'block';
+  });
+  
+element3.addEventListener('mouseout', () => {
+    encart3.style.display = 'none';
+  });
+
+  
+</script>
+    
 </body>
 
 </html>
