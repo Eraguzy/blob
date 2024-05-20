7f041a81e26d6340c16abc7a58f015f6c29258f1
Loueva <133343765+louevaberanger@users.noreply.github.com>
2024-05-20 15:11:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,4 @@
 function linkopener(a){
     window.open(a,'_self');
 }
-
-const element1 = document.querySelector('.decouverte');
-const encart1 = document.querySelector('.encart1');
-
-const element2 = document.querySelector('.classique');
-const encart2 = document.querySelector('.encart2');
-
-const element3 = document.querySelector('.vip');
-const encart3 = document.querySelector('.encart3');
-
-element1.addEventListener('mouseover', () => {
-  encart1.style.display = 'block';
-});
-
-element1.addEventListener('mouseout', () => {
-  encart1.style.display = 'none';
-});
-
-element2.addEventListener('mouseover', () => {
-    encart2.style.display = 'block';
-  });
-  
-  element2.addEventListener('mouseout', () => {
-    encart2.style.display = 'none';
-  });
-
-
-element3.addEventListener('mouseover', () => {
-    encart3.style.display = 'block';
-  });
-  
-  element3.addEventListener('mouseout', () => {
-    encart3.style.display = 'none';
-  });
   
