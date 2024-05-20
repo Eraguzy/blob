ea1090b52bb08e70511858d602e0f904ad865427
Loueva <133343765+louevaberanger@users.noreply.github.com>
2024-05-20 21:52:42 +0200

    Add files via upload

---
+++ liste_vues.php
@@ -0,0 +1,58 @@
+<?php
+$jsonFile = "compte.json";
+
+function loadJson($filePath) {
+    if (!file_exists($filePath)) {
+        return false;
+    }
+    $json = file_get_contents($filePath);
+    return json_decode($json, true);
+}
+
+$data = loadJson($jsonFile);
+
+if ($data === false) {
+    die("Erreur : Impossible de charger le fichier JSON.");
+}
+
+$userID = $_COOKIE['user_id'];
+
+$vues = [];
+
+foreach ($data['profils'] as $profil) {
+    if ($profil['id'] === $userID) {
+        $vues = $profil['stalkers'];
+        break;
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="styles/bloque.css">
+    <link rel="icon" href="logo.png">
+    <title>Liste des utilisateurs bloqués</title>
+</head>
+<body>
+    <nav class="bandeau">
+        <img src="logo.png" class="img">
+        <div class="bandeautitle">BLOB</div>
+        <div class="titrebandeau">Liste des utilisateurs ayant vu mon profil</div>
+        <input type="button" class="bouton" value="Retour" onclick="linkopener('abonne.php')" />
+    </nav>
+    <div class="Connexion-page">
+        <div class="Connexion-boite">
+            <h2>Mes stalkers :</h2>
+            <ul>
+                <?php foreach ($vues as $vuesID) : ?>
+                    <li><?php echo $vuesID; ?></li>
+                <?php endforeach; ?>
+            </ul>
+        </div>
+    </div>
+    <script src="script.js" type="text/javascript"></script>
+</body>
+</html>
