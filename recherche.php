440deeda3ed5a79b8c2490b99db754c5f3543758
Lucas Gournay <133283871+Spydertnt@users.noreply.github.com>
2024-05-24 13:46:37 +0200

    bug recherche

--- recherche.php
+++ recherche.php
@@ -1,8 +1,14 @@
 <?php
 $q = $_REQUEST["q"];
+if(isset($_REQUEST["filtre"]) && $_REQUEST["filtre"] != ""){
+    $filter = $_REQUEST["filtre"];
+} else {
+    $filter = "pseudo";
+}
+
 $res = "";
 $data = json_decode(file_get_contents('compte.json'), true);
-$limitResults = isset($_GET['limit']) ? $_GET['limit'] : false; // Vérifie si le paramètre 'limit' est défini
+$limitResults = isset($_GET['limit']) ? $_GET['limit'] : false;
 
 $count = 0;
 
@@ -10,13 +16,19 @@ if ($q !== "") {
     $q = strtolower($q);
     $len = strlen($q);
     foreach ($data['profils'] as $profil) {
-        if (stristr($q, substr($profil['pseudo'], 0, $len))) {
-            // Vérifie si la limite des résultats est activée et si le nombre maximum de résultats n'est pas atteint
+        // Vérifier si le profil correspond au filtre sélectionné
+        if (stristr($profil[$filter], $q)) {
             if (!$limitResults || ($limitResults && $count < 5)) {
                 if ($res === "") {
-                    $res = '<div class="profile" data-user-id="' . $profil['id'] . '">' . '<img src="photo_profil_utilisateurs/' . $profil['id'] . '.jpg" alt="Photo de profil">' . '<span>' . $profil['pseudo'] . '</span>' . '</div>';
+                    $res = '<div class="profile" data-user-id="' . $profil['id'] . '">' . 
+                           '<img src="photo_profil_utilisateurs/' . $profil['id'] . '.jpg" alt="Photo de profil">' . 
+                           '<span>' . $profil['pseudo'] . '</span>' . 
+                           '</div>';
                 } else {
-                    $res .= '<div class="profile" data-user-id="' . $profil['id'] . '">' . '<img src="photo_profil_utilisateurs/' . $profil['id'] . '.jpg" alt="Photo de profil">' . '<span>' . $profil['pseudo'] . '</span>' . '</div>';
+                    $res .= '<div class="profile" data-user-id="' . $profil['id'] . '">' . 
+                            '<img src="photo_profil_utilisateurs/' . $profil['id'] . '.jpg" alt="Photo de profil">' . 
+                            '<span>' . $profil['pseudo'] . '</span>' . 
+                            '</div>';
                 }
                 $count++;
             } else {
