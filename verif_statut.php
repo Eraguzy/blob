02c337bde84648492d4dc5b95bb9f0c62db5e141
Loueva <133343765+louevaberanger@users.noreply.github.com>
2024-05-24 16:59:44 +0200

    commentaires

--- verif_statut.php
+++ verif_statut.php
@@ -1,22 +1,26 @@
 <?php
+//on attribue  le temps correspondant aux statuts
 $statuts = [
     'decouverte' => 30,
     'classique' => 60,
     'vip' => 150,
     'admin' => 2000000
 ];
+//début session, ouverture fichier, lecture du fichier, récupération du cookie utilisateur
 session_start();
 $fichier = "compte.json";
 $json_content = file_get_contents($fichier);
 $data = json_decode($json_content, true);
 $user_id = $_COOKIE['user_id'];
 
+//fonction qui détermine si le statut est expiré en récupérant le statut de l'utilisateur
 function isStatutExpired($statut, $startTime, $statuts) {
     $currentTime = time();
     $duration = $statuts[$statut];
     return ($currentTime - $startTime) > $duration;
 }
 
+//fonction qui retourne le profil de l'utilisateur actuel
 function getUserProfile($user_id, $data) {
     foreach ($data['profils'] as $profile) {
         if ($profile['id'] == $user_id) {
@@ -27,8 +31,10 @@ function getUserProfile($user_id, $data) {
 }
 
 $response = [];
+//récupérer le profil
 $profile = getUserProfile($user_id, $data);
 
+//si le statut est expiré ou non, rediriger vers le fichier php correspondant (les admin ne sont concernés)
 if ($profile) {
     $statut = $profile['statut'];
     $startTime = $profile['statut_starter_time'];
