b0973a8ad4de9d5d5e621a2ad3e2dbd88f1cc1e3
Lucas Gournay <133283871+Spydertnt@users.noreply.github.com>
2024-05-26 19:19:59 +0200

    Ajout commentaires
    ajout commentaires page_recherche

--- php/page_recherche.php
+++ php/page_recherche.php
@@ -1,11 +1,13 @@
 <?php
-// Vérification si le cookie existe
+//On regarde si l'utilisateur est connecté grâce au cookie avec l'id utilisateur
 if (isset($_COOKIE['user_id'])) {
     if (!isset($_GET['q'])) {
+        //Si il n'y a pas d'argument q on redirige vers la page d'accueil
         header("Location: accueil.php");
         exit;
     }
 } else {
+    //Si il n'est pas connecté il est redirigé vers la page de connexion
     header("Location: ../php/page_connexion.php");
     exit;
 }
@@ -15,6 +17,7 @@ if (isset($_COOKIE['user_id'])) {
 <html>
 
 <head>
+    <!-- CSS, icône, titre de page -->
     <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="../styles/page_recherche.css">
     <link rel="icon" href="../images/logo.png">
@@ -22,6 +25,7 @@ if (isset($_COOKIE['user_id'])) {
 </head>
 
 <body>
+    <!-- Bandeau de page avec le bouton de redirection pour l'accueil-->
     <nav class="bandeau">
         <img src="../images/logo.png" class="img">
         <div class="bandeautitle">BLOB</div>
@@ -32,9 +36,9 @@ if (isset($_COOKIE['user_id'])) {
     <div class="Connexion-page">
         <div class="Connexion-boite">
             <div class="conteneur">
+                <!-- Barre de recherche avec filtre -->
                 <form action="#" method="get" class="recherche">
-                    <input type="text" name="q" id="recherche" placeholder="Recherche" value="<?php echo $_GET['q']; ?>"
-                        onkeyup="Suggestions(this.value)">
+                    <input type="text" name="q" id="recherche" placeholder="Recherche" value="<?php echo $_GET['q']; ?>" onkeyup="Suggestions(this.value)">
                     <select name="filtre">
                         <option value="">Pseudo</option>
                         <option value="nom">Nom</option>
@@ -51,16 +55,19 @@ if (isset($_COOKIE['user_id'])) {
                     </select>
                     <button type="submit">Rechercher</button>
                 </form>
+                <!-- C'est la où les résultats s'affichent -->
                 <div id="res"></div>
             </div>
         </div>
     </div>
 
     <script>
+        //Redirige l'utilisateur vers la page de profil de l'utilisateur ciblé
         function viewProfile(id_utilisateur) {
             window.location.href = 'page_profil.php?id_utilisateur=' + id_utilisateur;
         }
 
+        //Récupère le filtre et recherche des profils correspondant à ce qui est tappé dans la barre de recherche avec le filtre choisi
         function Suggestions(str) {
             var filtre = document.querySelector('select[name="filtre"]').value; // Récupérer la valeur sélectionnée du champ select
             var xhttp;
@@ -71,9 +78,10 @@ if (isset($_COOKIE['user_id'])) {
             xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function () {
                 if (this.readyState == 4 && this.status == 200) {
+                    //Remplace le vide par le résultat de la recherche
                     document.getElementById("res").innerHTML = this.responseText;
-
-                    // Ajouter un gestionnaire d'événements de clic pour chaque profil
+                    //Gestionnaire d'événements de clic pour chaque profil
+                    //Lorsque l'utilisateur clique sur un profil, il est redirigé vers la page du profil en question
                     var profileElements = document.querySelectorAll(".profile");
                     profileElements.forEach(function (element) {
                         element.addEventListener('click', function () {
@@ -83,10 +91,12 @@ if (isset($_COOKIE['user_id'])) {
                     });
                 }
             };
+            //On envoie le champ de la barre de recherche, le filtre et si il y a une limite au nombre de résultats ou non à un algorithme de recherche
             xhttp.open("GET", "recherche.php?q=" + str + "&filtre=" + filtre);
             xhttp.send();
         }
 
+        //Ajuste la taille de la boite en fonction des résultats
         window.addEventListener('load', function () {
             var Taille_boite = document.querySelector('.Connexion-boite').offsetHeight;
             var Taille_barre = document.querySelector('.recherche').offsetHeight;
@@ -97,8 +107,6 @@ if (isset($_COOKIE['user_id'])) {
             document.getElementById('res').style.paddingRight = Largeur_boite_resultats + 'px';
         });
     </script>
-    </div>
-    </div>
 
     <script src="../scripts/script.js" type="text/javascript"></script>
 </body>
